f5b00b2e96f9c653c5793fdb090ba7ad22b82ea5
mbeckdev <mbeckdev42@gmail.com>
2022-04-03 17:32:10 -0600

    Add age container

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="background__art-box"></div>
-      <form>
+      <form className="form">
+        <div className="form__background-art-box"></div>
         <h1>Search for a rental car</h1>
         <label htmlFor="location">Pick up location</label>
         <input
@@ -17,6 +17,33 @@ function App() {
         <label htmlFor="deliver-elsewhere">
           Deliver the car somewhere else
         </label>
+        <div className="dates-container">
+          <div className="date">
+            <label htmlFor="date__pickup">Date of pickup</label>
+            <input
+              // type="pick-date"
+              type="text"
+              id="date-pickup"
+              placeholder="Sat 26 Mar 2022"
+              className="input-field"
+            />
+            <input type="text" placeholder="10" className="input-field" />
+            <input type="text" placeholder="00" className="input-field" />
+          </div>
+        </div>
+        <div className="age-container">
+          <input
+            type="checkbox"
+            d="car-search__is-between-30-and-60"
+            className="age__input"
+          />
+          <label htmlFor="car-search__is-between-30-and-60">
+            The driver's age between 30 and 65?
+          </label>
+          <a href="#" className="age-info">
+            <div>i</div>
+          </a>
+        </div>
       </form>
     </div>
   );
